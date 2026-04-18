f22aaa249cd50036fa0a5d4afcd926a7a952990e
dhenwood <dhenwood@users.noreply.github.com>
2026-04-18 16:20:47 +1000

    Updated macro name on import

--- SendMessage.js
+++ SendMessage.js
@@ -1,5 +1,5 @@
 import xapi from 'xapi';
-import {getValidAccessToken} from './OAuth Refresh Tokens';
+import {getValidAccessToken} from './OAuthRefreshTokens';
 
 async function sendMessage() {
   const token = await getValidAccessToken();
