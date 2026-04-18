0c8fe5a4ba22caa48a89affeb1a3a420ee702f7f
dhenwood <dhenwood@users.noreply.github.com>
2026-04-18 16:17:58 +1000

    Create SendMessage.js

---
+++ SendMessage.js
@@ -0,0 +1,29 @@
+import xapi from 'xapi';
+import {getValidAccessToken} from './OAuth Refresh Tokens';
+
+async function sendMessage() {
+  const token = await getValidAccessToken();
+
+  let email = "dhenwood@cisco.com"
+  let msg = "Successfully sent message using OAuth token: " + token
+  let finalMsg = JSON.stringify({
+    "toPersonEmail": email,
+    "text": msg
+  })
+
+  let url = 'https://webexapis.com/v1/messages'
+  let header = [
+    `Authorization: Bearer ${token}`,
+    'Content-Type: application/json'
+  ]
+
+  xapi.Command.HttpClient.Post({
+      AllowInsecureHTTPS: "True",
+      Header: header,
+      ResultBody: "PlainText",
+      Url: url
+    },
+    finalMsg);
+}
+
+sendMessage()
